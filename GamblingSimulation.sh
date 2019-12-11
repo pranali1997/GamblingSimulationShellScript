dc426f6f32701bc5a482fe188c8b7178e9a40b5b
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-11 13:19:54 +0530

    Added : function to calculate if gambler win or loss upto daily limit

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -4,13 +4,26 @@ echo "WELCOME TO GAMBLING SIMULATION"
 
 STAKE_PER_DAY=100
 BET=1
+MAX_STAKE=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))
+MIN_STAKE=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
 
 #variables
-winOrLoss=$((RANDOM%2))
+cash=$STAKE_PER_DAY
 
-if [ winOrLoss -eq 1]
-then
-	echo "win"
-else
-	echo "loss"
-fi
+function dailyPlay()
+{
+	while [ $cash -gt $MIN_STAKE ] && [ $cash -lt $MAX_STAKE ]
+	do
+		random=$((RANDOM%2))
+		if [ $random == 1 ]
+		then
+			cash=$(($cash+$BET))
+			echo $cash
+		else
+			cash=$(($cash-$BET))
+			echo $cash
+		fi
+	done
+	echo $cash
+}
+dailyPlay
