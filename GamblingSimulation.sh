7e2d5adfa176a94ed2573296929ef450c40e223f
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-11 16:11:21 +0530

    Added : function for calculating amount obtained after playing for 20 days

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -6,10 +6,12 @@ STAKE_PER_DAY=100
 BET=1
 MAX_STAKE=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))
 MIN_STAKE=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
+DAYS=20
 
 #variables
 cash=$STAKE_PER_DAY
-
+gainAmount=0
+totalProfit=0
 function dailyPlay()
 {
 	while [ $cash -gt $MIN_STAKE ] && [ $cash -lt $MAX_STAKE ]
@@ -18,12 +20,29 @@ function dailyPlay()
 		if [ $random == 1 ]
 		then
 			cash=$(($cash+$BET))
-			echo $cash
 		else
 			cash=$(($cash-$BET))
-			echo $cash
 		fi
 	done
-	echo $cash
+
+	gainAmount=$(($cash-100))
+	echo $gainAmount
 }
-dailyPlay
+
+function profitOfParticularDays()
+{
+        local day=1
+        while [ $day -lt $DAYS ]
+        do
+                local profitOfTheDay=0
+                profitOfTheDay=$(dailyPlay)
+                dayProfit[$day]=$gainAmount
+                day=$(($day+1))
+                totalProfit=$(($totalProfit+$profitOfTheDay))
+        done
+        echo "total profit :"$totalProfit
+}
+profitOfParticularDays
+
+
+
